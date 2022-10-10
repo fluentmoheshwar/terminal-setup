893d10e09433cef53c016f084f3e2d50a9442b3d
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2022-10-10 19:01:22 +0600

    Added PowerType

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 oh-my-posh --init --shell pwsh --config ~\ohmyposh.json | Invoke-Expression
 Import-Module PSReadLine
-Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
+Enable-PowerType
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 Import-Module -Name Terminal-Icons
